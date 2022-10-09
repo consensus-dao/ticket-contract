01912ee09b0d95f6f2ce0e5de3ee63dd33f62f1d
davidtnfsh <davidtnfsh@gmail.com>
2022-10-09 20:51:50 +0800

    <feat>(ticket): finished

--- contracts/InPersonTicketNFT.sol
+++ contracts/InPersonTicketNFT.sol
@@ -10,17 +10,20 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract InPersonTicketNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-
+    // TODO: DaoTicketAddress is just a place holder for now!
+    address DaoTicketAddress = 0x78000b0605E81ea9df54b33f72ebC61B5F5c8077;
     constructor() ERC721("InPersonTicketNFT", "NFT") {}
     event NftEvent(uint256 tokenId, string tokenURI);
-    function mintNFT(address recipient, string memory tokenURI)
+    function mintNFT(address recipient)
         public onlyOwner
         returns (uint256)
     {
+        require(msg.sender == DaoTicketAddress, "Only DAO ticket contract can mint an InPersonTicketNFT!");
         _tokenIds.increment();
-
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
+        // TODO: put a tokenURI placeholder for now!
+        string memory tokenURI = "ipfs://QmNVJPswnRwHReptaBSrW81R43khRqDAdUMoZEtdnhM4mn";
         _setTokenURI(newItemId, tokenURI);
         emit NftEvent(newItemId, tokenURI);
         return newItemId;
--- contracts/ticket.sol
+++ contracts/ticket.sol
@@ -3,47 +3,53 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./InPersonTicketNFT.sol";
 
 contract Ticket is Ownable {
     using SafeERC20 for IERC20;
     IERC20 public ohm;
+    address public ohmAddr;
     IERC20 public usdt;
+    address public usdtAddr;
     IERC20 public usdc;
+    address public usdcAddr;
     IERC20 public frax;
+    address public fraxAddr;
     mapping (string => uint) public usdTicketPrices;
     mapping (string => uint) public ohmTicketPrices;
     string[] public ticketTypes;
+    address public gnosisMultiSig = 0x7EE54ab0f204bb3A83DF90fDd824D8b4abE93222;
+    address public InPersonTicketNFTAddr;
+
     // token = MyToken's contract address
     constructor() {
         ohm = IERC20(0x64aa3364F17a4D01c6f1751Fd97C2BD3D7e7f1D5);
+        ohmAddr = 0x64aa3364F17a4D01c6f1751Fd97C2BD3D7e7f1D5;
         usdt = IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+        usdtAddr = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
         usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+        usdcAddr = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
         frax = IERC20(0x853d955aCEf822Db058eb8505911ED77F175b99e);
+        fraxAddr = 0x853d955aCEf822Db058eb8505911ED77F175b99e;
     }
 
     // Modifier to check token allowance
     modifier checkAllowance(address _tokenAddr, string memory ticketName, bool isStableCoin) {
-        uint256 tokenPrice = getTicketPrice(ticketName, isStableCoin);
+        uint256 tokenPrice = _getTicketPrice(_tokenAddr, ticketName, isStableCoin);
         IERC20 _token;
-        if( _tokenAddr == 0x64aa3364F17a4D01c6f1751Fd97C2BD3D7e7f1D5) {
+        if( _tokenAddr == ohmAddr) {
             _token = ohm;
-        } else if(_tokenAddr == 0xdAC17F958D2ee523a2206206994597C13D831ec7){
+        } else if(_tokenAddr == usdtAddr){
             _token = usdt ;
-        } else if(_tokenAddr == 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48){
+        } else if(_tokenAddr == usdcAddr){
             _token = usdc ;
-        } else if(_tokenAddr == 0x853d955aCEf822Db058eb8505911ED77F175b99e){
+        } else if(_tokenAddr == fraxAddr){
             _token = frax ;
         }
         require(_token.allowance(msg.sender, address(this)) >= tokenPrice, "Error");
         _;
     }
 
-    function getTicketPrice(string memory ticketName, bool isStableCoin) public view returns (uint) {
-        if (isStableCoin == true){
-            return usdTicketPrices[ticketName];
-        }
-        return ohmTicketPrices[ticketName];
-    }
     
     function setTicketPrice(string memory ticketName, bool isStableCoin, uint ticketPrice) public onlyOwner {
         if (isStableCoin == true){
@@ -54,8 +60,9 @@ contract Ticket is Ownable {
 
     function buyTicket(address _tokenAddr, string memory ticketName, bool isStableCoin) public checkAllowance(_tokenAddr, ticketName, isStableCoin) {
         IERC20 token = IERC20(_tokenAddr);
-        uint256 tokenPrice = getTicketPrice(ticketName, isStableCoin);
+        uint256 tokenPrice = _getTicketPrice(_tokenAddr, ticketName, isStableCoin);
         token.safeTransferFrom(msg.sender, address(this), tokenPrice);
+        InPersonTicketNFT(InPersonTicketNFTAddr).mintNFT(msg.sender);
     }
     
     function withdrawToken() external onlyOwner {
@@ -64,10 +71,32 @@ contract Ticket is Ownable {
         uint256 usdtBalance = usdt.balanceOf(address(this));
         uint256 usdcBalance = usdc.balanceOf(address(this));
         uint256 fraxBalance = frax.balanceOf(address(this));
-        address gnosisMultiSig = 0x7EE54ab0f204bb3A83DF90fDd824D8b4abE93222;
         ohm.safeTransfer(gnosisMultiSig, ohmBalance);
         usdt.safeTransfer(gnosisMultiSig, usdtBalance);
         usdc.safeTransfer(gnosisMultiSig, usdcBalance);
         frax.safeTransfer(gnosisMultiSig, fraxBalance);
+    }
+    function _getTicketPrice(address _tokenAddr, string memory ticketName, bool isStableCoin) public view returns (uint) {
+        if (isStableCoin == true){
+            return usdTicketPrices[ticketName] * _getTokenDecimals(_tokenAddr);
+        }
+        return ohmTicketPrices[ticketName] * _getTokenDecimals(_tokenAddr);
+    }
+
+    function setInPersonTicketNFTAddr(address nftAddr) external onlyOwner {
+        InPersonTicketNFTAddr = nftAddr;
+    }
+
+    function _getTokenDecimals(address _tokenAddr) public view returns (uint) {
+        if (_tokenAddr == ohmAddr) {
+            return 9;
+        } else if (_tokenAddr == usdtAddr) {
+            return 6;
+        } else if (_tokenAddr == usdcAddr) {
+            return 6;
+        } else if (_tokenAddr == fraxAddr) {
+            return 18;
+        } 
+        revert("token Address not found!");
     }
 }
