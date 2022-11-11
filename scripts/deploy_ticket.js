f37282c3948e1ac53464b972fd3651f74a143d4f
davidtnfsh <davidtnfsh@gmail.com>
2022-11-11 22:22:18 +0800

    <chores>: update production contract addr

--- scripts/deploy_ticket.js
+++ scripts/deploy_ticket.js
@@ -1,7 +1,7 @@
 async function main() {
   // remember to update addresses of ohm, frax, dai and usdc!
   const Ticket = await ethers.getContractFactory("Ticket");
-  const ticket = await Ticket.deploy("0x78000b0605E81ea9df54b33f72ebC61B5F5c8077", "0x0000000000000000000000000000000000000000", "0xD635a0d726B9bd390eE9684eb758Db0bb2938E05", "0xD635a0d726B9bd390eE9684eb758Db0bb2938E05", "0xD635a0d726B9bd390eE9684eb758Db0bb2938E05");
+  const ticket = await Ticket.deploy("0xFBEb8FCcaDB61D4219864E80F95dc9E7DC0a1596", "0x0000000000000000000000000000000000000000", "0x8D9bA570D6cb60C7e3e0F31343Efe75AB8E65FB1", "0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F", "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1");
   ticket.deployed();
   console.log("Ticket Contract deployed to address:", ticket.address);
 
@@ -17,7 +17,6 @@ async function main() {
   await ticket.setTicketPrice("2022-in-person", true, ethers.utils.parseUnits("40", 18));
   await ticket.setTicketPrice("2022-in-person-contributor", true, ethers.utils.parseUnits("20", 18));
   await ticket.setTicketPrice("2022-in-person", false, ethers.utils.parseUnits("0.006", 18));
-  await ticket.setTicketPrice("2022-in-person-contributor", false, ethers.utils.parseUnits("0.003", 18));
   await ticket.setTicketPrice("2022-in-person-contributor", false, ethers.utils.parseUnits("0.003", 18));
   console.log('Finished!');
 }
--- scripts/test_3_withdraw.js
+++ scripts/test_3_withdraw.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const ethers = require('ethers');
 const ethersProvider = new ethers.providers.JsonRpcProvider(process.env.API_URL);
 const signer = new ethers.Wallet(process.env.PRIVATE_KEY, ethersProvider);
-const contract = '0x34e2C75a4C106C74F1b8bbCAA7A525e5ECE063EC'
+const contract = '0xEe8C4781139286c248B9E81C8ca5C850d6c2c9A7'
 const ticketContract = new ethers.Contract(contract, require('../artifacts/contracts/Ticket.sol/Ticket.json').abi, signer);
 
 (async function main() {
