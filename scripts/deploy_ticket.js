8186ce02ca29f701814f83db49457cc6df69f4f5
davidtnfsh <davidtnfsh@gmail.com>
2022-11-11 21:07:07 +0800

    <feat>: add ticket inventories

--- scripts/deploy_ticket.js
+++ scripts/deploy_ticket.js
@@ -1,15 +1,15 @@
 async function main() {
-  const ethers = require('ethers');
   // remember to update addresses of ohm, frax, dai and usdc!
   const Ticket = await ethers.getContractFactory("Ticket");
-  const ticket = await Ticket.deploy("0x78000b0605E81ea9df54b33f72ebC61B5F5c8077", "0x0000000000000000000000000000000000000000", "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1", "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1", "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1")
+  const ticket = await Ticket.deploy("0x78000b0605E81ea9df54b33f72ebC61B5F5c8077", "0x0000000000000000000000000000000000000000", "0xD635a0d726B9bd390eE9684eb758Db0bb2938E05", "0xD635a0d726B9bd390eE9684eb758Db0bb2938E05", "0xD635a0d726B9bd390eE9684eb758Db0bb2938E05");
   ticket.deployed();
-  console.log("Ticket Contract deployed to address:", ticket.address)
+  console.log("Ticket Contract deployed to address:", ticket.address);
 
-  const InPersonTicketNFT = await ethers.getContractFactory("InPersonTicketNFT")
-  const inPersonTicketNFT = await InPersonTicketNFT.deploy(ticket.address)
-  console.log("InPersonTicketNFT Contract deployed to address:", inPersonTicketNFT.address)
+  const InPersonTicketNFT = await ethers.getContractFactory("InPersonTicketNFT");
+  const inPersonTicketNFT = await InPersonTicketNFT.deploy(ticket.address);
+  console.log("InPersonTicketNFT Contract deployed to address:", inPersonTicketNFT.address);
   inPersonTicketNFT.deployed();
+  // there's only 40 seats in AppWorks!
   await inPersonTicketNFT.setTicketInventories(40);
   // set NFT addr
   await ticket.setInPersonTicketNFTAddr(inPersonTicketNFT.address);
